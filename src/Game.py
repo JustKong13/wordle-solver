d215f344648b404ee3c00e4fb48f2b1e87f5a5ec
Justin Kong <justinkong24@gmail.com>
2023-06-18 23:32:35 -0400

    working word validator

--- src/Game.py
+++ src/Game.py
@@ -1,19 +1,18 @@
 # The engine game of wordle
 import random
 
-global PLAYING
-global WIN
-global LOSE
-
 
 class Wordle:
     def __init__(self):
         self.board = [[] * 5] * 6
         self.guesses = []
         self.answer = self.generateAnswer()
-        self.gameState = PLAYING
+        self.validGuesses = self.generateValidGuesses()
 
     def generateAnswer(self):
+        """
+        Returns a word to be the answer to the game
+        """
         randomIndex = random.randint(0, 2310)
         validWords = '../wordlists/WordleWords.txt'
 
@@ -22,10 +21,37 @@ class Wordle:
 
         return lines[randomIndex].upper()
 
-    def correctWord(self, answer, guess):
-        self.guesses.append(guess)
-        row = ['' * 5]
+    def generateValidGuesses(self):
+        """
+        Returns a list of all guessable words
+        """
+        validGuesses = '../wordlists/validGuesses.txt'
+        with open(validGuesses) as f:
+            words = f.readlines()
 
-        for i in range(5):
-            if guess[i] == answer[i]:
-                row[i] == 'G'
+        validGuesses = []
+
+        for word in words:
+            commaIndex = word.index(',')
+            validGuesses.append(word[commaIndex + 1: -1].upper())
+
+        return validGuesses
+
+    def evaluateGuess(self, word, guess):
+        if not (guess.upper() in self.validGuesses):
+            return "INVALID"
+
+        row = ['', '', '', '', '']
+        print(row)
+        guessed_index_of_letter = set()
+        for i in range(len(word)):
+            if word[i] == guess[i]:
+                row[i] = "G"
+                guessed_index_of_letter.add(i)
+
+            elif (word[i] in guess) and (guess[i] != word[i]) and (i not in guessed_index_of_letter):
+                row[i] = "Y"
+
+            else:
+                row[i] = "B"
+        return row
