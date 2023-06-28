5cc0a04b92ba19dcba4092c8b0c10bd679cfa882
Justin Kong <justinkong24@gmail.com>
2023-06-27 23:12:10 -0400

    initalizing bot

--- src/Bot.py
+++ src/Bot.py
@@ -3,3 +3,12 @@ from Game import Wordle
 
 Wordle = Wordle()
 guess_bank = Wordle.validGuesses
+
+
+with open('./wordlists/WordleWords.txt') as f:
+    all_words = f.readlines()
+f.close()
+
+possible_answers = []
+for word in all_words:
+    possible_answers.append(word[:-1])
